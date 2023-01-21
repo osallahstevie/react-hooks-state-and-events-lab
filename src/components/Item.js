3060291e5d8bbf3360eb6be67fc108acb01831e3
osallahstevie <osallahstevie@gmail.com>
2023-01-21 23:53:52 +0300

    final commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  // state variable to keep track of whether the item is in the cart or not
+  const [inCart, setInCart] = useState(false);
+
+  // use state variable to determine className for li element
+  const liClass = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+        className="add"
+        onClick={() => setInCart(!inCart)}>
+          {inCart ? 'Remove From Cart' : 'Add to Cart'}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  
+  // state variable to keep track of selected category
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  // filter items based on selected category
+  const filteredItems =
+    selectedCategory === "All"
+      ? items
+      : items.filter((item) => item.category === selectedCategory);
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select 
+          name="filter" 
+          onChange={(e) => setSelectedCategory(e.target.value)}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
@@ -22,3 +34,4 @@ function ShoppingList({ items }) {
 }
 
 export default ShoppingList;
+
